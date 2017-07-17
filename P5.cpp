56f6fc40a7f3b826d8149278560ea4a1b95d6ba9
Thomas Finn <thomasjf@usc.edu>
2017-07-17 15:41:16 -0400

    Create P5.cpp
    Needs to be optimized using prime expansion

---
+++ P5.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <math.h>
+#include <string>
+
+using namespace std;
+
+/*
+    What is the smallest positive number that is 
+    evenly divisible by all of the numbers from 1 to 20?
+*/
+bool divisible(int m, int x){
+    for(int i = x; i > 1; i--){
+        if(m%i) return false;
+    }
+    return true;
+}
+
+int sMult(int x){
+    int start = x*(x-1);
+    while(!divisible(start, x)){
+        start+=2;
+    }
+    return start;
+}
+
+int main(){
+    cout << sMult(20) << endl;
+    return 0;
+}
